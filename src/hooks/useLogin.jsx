38525f8a053a96380fb1cf7ea72f42f0aea37172
Oleksandr Dziunyk <alexdziunyk@gmail.com>
2023-06-21 02:58:32 +0300

    debugging

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -10,7 +10,7 @@ export const useLogin = () => {
         setIsLoading(true)
         setError(null)
         console.log(1)
-        const response = await fetch(`${process.env.REACT_APP_API_URL}api/user/login`, {
+        const response = await fetch(`/api/user/login`, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({login, password})
