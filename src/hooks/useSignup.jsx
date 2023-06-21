9c882d64d5078da0702e770c996ee10296d3c07c
Oleksandr Dziunyk <alexdziunyk@gmail.com>
2023-06-22 01:58:07 +0300

    experimenting with url

--- src/hooks/useSignup.jsx
+++ src/hooks/useSignup.jsx
@@ -10,7 +10,7 @@ export const useSignup = () => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch(`/api/user/signup`, {
+        const response = await fetch(`https://mern-textblog-44104543a6bf.herokuapp.com/api/user/signup`, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({login, password})
